7f1f1309967c7f9376a40979b5af2b6bddf872ad
OnePercent <ali.nubeebukus@gmail.com>
2022-06-17 10:35:51 +0400

    had  bug  had to redo the whole logic
    Calculator wasnt chaining operators and presenting results correctly
    fixed everything+clear button works too

--- javascript.js
+++ javascript.js
@@ -22,7 +22,7 @@ const display=document.querySelector('.display');
 const active =document.querySelector('.active');
 buttons.forEach(button=>{
     button.addEventListener('click',()=>{
-        if(displayData.active===1){
+        if(displayData.active){
             display.textContent='';
             displayData.active=0;
         }
@@ -33,31 +33,36 @@ let displayData={
     firstNumber:null,
     secondNumber:null,
     operator:null,
-    active:0
+    active:0,
+    equalactive:0
 }
 const operators=document.querySelectorAll('.operator')
 operators.forEach(operator=>{
     operator.addEventListener('click',()=>{
+    if(displayData.equalactive!==1){
+        if(displayData.active===1)return;
+        if(displayData.firstNumber===null){displayData.firstNumber=Number(display.textContent);}
+        else{
+            displayData.secondNumber=Number(display.textContent);
+            display.textContent=operate(displayData.firstNumber,displayData.operator,displayData.secondNumber);;
+            displayData.firstNumber=Number(display.textContent);
+        }}
         if(operator.textContent==='+'){displayData.operator=add;}
         else if(operator.textContent==='-'){displayData.operator=subtract;}
         else if(operator.textContent==='*'){displayData.operator=multiply;}
         else if(operator.textContent==='/'){displayData.operator=divide;}
+        // displayData.active ===1 ? displayData.firstNumber=display.textContent: displayData.secondNumber;
         displayData.active=1;
-        if(displayData.firstNumber===null){
-        displayData.firstNumber=Number(display.textContent);}
-        else{
-            displayData.secondNumber=Number(display.textContent);
-            display.textContent=operate(displayData.firstNumber,displayData.operator,displayData.secondNumber);
-            displayData.firstNumber=operate(displayData.firstNumber,displayData.operator,displayData.secondNumber);
-            }
+        displayData.equalactive=0;
 })
     })
 const equal=document.querySelector('.equal');
 equal.addEventListener('click',()=>{
+    if(displayData.equalactive===1)return;
     displayData.secondNumber=Number(display.textContent);
     display.textContent=operate(displayData.firstNumber,displayData.operator,displayData.secondNumber);
-    displayData.firstNumber=null;
-    displayData.active=1;
+    displayData.firstNumber=Number(display.textContent);
+    displayData.equalactive=1;
 })
 const clear=document.querySelector('.clear');
 clear.addEventListener('click',()=>{
@@ -65,4 +70,5 @@ clear.addEventListener('click',()=>{
     displayData.secondNumber=null;
     displayData.operator=null;
     displayData.active=0;
+    displayData.equalactive=0;
     display.textContent='';})
